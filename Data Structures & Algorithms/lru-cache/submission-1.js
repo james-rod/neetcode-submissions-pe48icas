34cad6dc6323a9b8c68214abd559014a9118e203
James Flores <74877516+james-rod@users.noreply.github.com>
2026-05-09 22:21:07 -0400

    Add: lru-cache - submission-1

---
+++ Data Structures & Algorithms/lru-cache/submission-1.js
@@ -0,0 +1,70 @@
+class Node {
+    constructor(key, val){
+        this.key = key
+        this.val = val
+        this.prev = null
+        this.next = null
+    }
+}
+
+class LRUCache {
+    /**
+     * @param {number} capacity
+     */
+    constructor(capacity) {
+        this.capacity = capacity
+        this.cache = new Map()
+        this.left = new Node(0, 0)
+        this.right = new Node(0,0)
+        this.left.next = this.right
+        this.right.prev = this.left
+    }
+
+    remove(node){
+        let prev = node.prev
+        let next = node.next
+        prev.next = next
+        next.prev = prev 
+    }
+
+    insert(node){
+        let prev = this.right.prev
+        prev.next = node 
+        node.prev = prev
+        node.next = this.right
+        this.right.prev = node
+    }
+
+    /**
+     * @param {number} key
+     * @return {number}
+     */
+    get(key) {
+        if(this.cache.has(key)){
+            let node = this.cache.get(key)
+            this.remove(node)
+            this.insert(node)
+            return node.val
+        }
+        return -1
+    }
+
+    /**
+     * @param {number} key
+     * @param {number} value
+     * @return {void}
+     */
+    put(key, value) {
+        if(this.cache.has(key)){
+            this.remove(this.cache.get(key))
+        }
+        let newNode = new Node(key, value)
+        this.cache.set(key, newNode)
+        this.insert(newNode)
+        if(this.cache.size > this.capacity){
+            let LRU = this.left.next
+            this.remove(LRU)
+            this.cache.delete(LRU.key)
+        }
+    }
+}
