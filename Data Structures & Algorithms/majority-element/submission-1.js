0245a5e066ef01fd7fbbad40f2b23a3eafadb6b6
James Flores <74877516+james-rod@users.noreply.github.com>
2026-05-14 22:15:01 -0400

    Add: majority-element - submission-1

---
+++ Data Structures & Algorithms/majority-element/submission-1.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    majorityElement(nums) {
+        let hashMap = new Map()
+        let res = 0
+        let maxCount = 0
+
+        for(let num of nums){
+            hashMap.set(num, (hashMap.get(num) || 0) + 1)
+
+            if(hashMap.get(num) > maxCount){
+                res = num
+                maxCount = hashMap.get(num)
+            }
+        }
+        return res
+    }
+}
