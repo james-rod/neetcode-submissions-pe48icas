4d259476df90ac5ac7a5bba0e17c32ffaab4a7d9
James Flores <74877516+james-rod@users.noreply.github.com>
2026-09-20 16:09:35 -0400

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        let left = 0;
+        let right = s.length - 1
+
+        while(left < right){
+            while(left < right && !this.isAlphanumeric(s[left])){
+                left++
+            }
+            while(left < right && !this.isAlphanumeric(s[right])){
+                right--
+            }
+
+            if(s[left].toLowerCase() !== s[right].toLowerCase()){
+                return false
+            }
+            left++
+            right--
+        }
+        return true
+        
+    }
+
+    isAlphanumeric(str){
+        return (
+            (str >= "A" && str <= "Z") ||
+            (str >= "a" && str <= "z") ||
+            (str >= "0" && str <= "9")
+        )
+    }
+}
